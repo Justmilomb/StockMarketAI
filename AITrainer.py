e06da36867e38ca931388757d0411d124da03a40
Milo <172209513+Justmilomb@users.noreply.github.com>
2025-03-12 22:40:37 +0000

    API Has limit will work on tommorow

--- AITrainer.py
+++ AITrainer.py
@@ -39,40 +39,47 @@ def MainMenu():
 	
 def LoadData():
 	
-	szAnswer1 = ""
+	szAnswer1 = "y"
 	szURL = ""
 	szData = ""
 	szDataNice = ""
 	szCOMPANY = "AAPL"
 	szINTERVAL = "1min"
 	szOUT_PUT_SIZE = "full"
-	szAPI_KEY = "ZH9X5NIRXZGZPJ3O"
+	szAPI_KEY = "AZBCZN11R13GKYWW"
 	szDATA_START = "2024-01"
+	OutPutFolder = "StockData"
+	os.makedirs(OutPutFolder, exist_ok=True)
+	
+	for year in range(2000, 2025 + 1):
+		for month in range(1, 13 + 1):
+			szDataStart = f"{year}-{month:02d}"
+			time.sleep(5)
 
-	szURL = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={szCOMPANY}&interval={szINTERVAL}&outputsize={szOUT_PUT_SIZE}&apikey={szAPI_KEY}&month={szDATA_START}&datatype=csv"
-	Response = requests.get(szURL)
-	csv_data = StringIO(Response.text)
-	szData = pd.read_csv(csv_data)
-	szData['Timestamp'] = pd.to_datetime(szData['timestamp'])
-	szDataNice = pd.DataFrame(szData)
-	szDataNice.columns = ["Timestap", "Open", "High", "Close", "Low", "Close", "Volume"]
-	print("Printing data for you...")
-	time.sleep(3) # Temporary replacment for progress bar
-	print(szDataNice)
-	while len(szAnswer1) == 0:
-		szAnswer1 = input("Would you like to save this data? 'y' or 'n':   ").strip().lower()
-		if szAnswer1 == "y":
-			print("Saving now...")
+			szURL = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={szCOMPANY}&interval={szINTERVAL}&outputsize={szOUT_PUT_SIZE}&apikey={szAPI_KEY}&month={szDataStart}&datatype=csv"
+			Response = requests.get(szURL)
+			csv_data = StringIO(Response.text)
+			szData = pd.read_csv(csv_data)
+			szDataNice = pd.DataFrame(szData)
+			szDataNice.columns = ["Timestamp", "Open", "High", "Low", "Close", "Volume"]
+			print("Printing data for you...")
 			time.sleep(3) # Temporary replacment for progress bar
-			szDataNice.to_csv("apple_stock_minute_data.csv")
-			print("Data saved successfully!")
-		elif szAnswer1 == "n":
-			print("Skipping saving the ai...")
-			time.sleep(1)
-		else:
-			szAnswer1 = ""
-			print("Invalid input try again...")
-			time.sleep(1.5)
+			print(szDataNice)
+			while len(szAnswer1) == 0:
+				szAnswer1 = input("Would you like to save this data? 'y' or 'n':   ").strip().lower()
+				if szAnswer1 == "y":
+					print("Saving now...")
+					time.sleep(3) # Temporary replacment for progress bar
+					file_name = f"{OutPutFolder}/{szCOMPANY}_{szDataStart}.csv"
+					szDataNice.to_csv(file_name, index=False)
+					print("Data saved successfully!")
+				elif szAnswer1 == "n":
+					print("Skipping saving the ai...")
+					time.sleep(1)
+				else:
+					szAnswer1 = ""
+					print("Invalid input try again...")
+					time.sleep(1.5)
 	
 	
 
